b2c557733dbd4d4734ba87a838e1ab05f052ddf4
Akshay Nadig N R <akshaynadig.nr@spurtreetech.com>
2023-08-08 17:55:27 +0530

    added dialog, store, form, input

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,25 @@
+"use client"
+
 import { Button } from '@/components/ui/button'
+import { Modal } from '@/components/ui/modal'
 import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
+import { useEffect } from 'react'
+import { useStoreModal } from '@/hooks/use-store-modal'
 
 export default function SetUpPage() {
-  return (
-<div className='p-4'>
-    <UserButton afterSignOutUrl="/"/>
-</div>
+    const onOpen = useStoreModal((state) => state.onOpen);
+    const isOpen = useStoreModal((state) => state.isOpen)
+
+    useEffect(() => {
+        if (!isOpen) {
+            onOpen()
+        }
+    }, [isOpen, onOpen])
+
+    return (
+        <div className='p-4'>
+            Root page
+        </div>
     )
 }
