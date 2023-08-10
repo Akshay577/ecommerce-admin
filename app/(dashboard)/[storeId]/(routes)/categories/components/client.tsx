76b8b5221359335142088db7953d5d7c6627d582
Akshay Nadig N R <akshaynadig.nr@spurtreetech.com>
2023-08-10 12:47:07 +0530

    added categories

--- app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -7,15 +7,16 @@ import { Button } from "@/components/ui/button";
 import { DataTable } from "@/components/ui/data-table";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
+import { ApiAlert } from "@/components/ui/api-alert";
 
-import { columns, BillboardColumn } from "./columns";
+import { columns, CategoryColumn } from "./columns";
 import { ApiList } from "@/components/ui/api-list";
 
-interface BillboardClientProps {
-  data: BillboardColumn[];
+interface CategoriesClientProps {
+  data: CategoryColumn[];
 }
 
-export const BillboardClient: React.FC<BillboardClientProps> = ({
+export const CategoriesClient: React.FC<CategoriesClientProps> = ({
   data
 }) => {
   const params = useParams();
@@ -24,16 +25,16 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({
   return (
     <>
       <div className="flex items-center justify-between">
-        <Heading title={`Billboards (${data.length})`} description="Manage billboards for your store" />
-        <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
+        <Heading title={`Categories (${data.length})`} description="Manage categories for your store" />
+        <Button onClick={() => router.push(`/${params.storeId}/categories/new`)}>
           <Plus className="mr-2 h-4 w-4" /> Add New
         </Button>
       </div>
       <Separator />
-      <DataTable searchKey="label" columns={columns} data={data} />
-      <Heading title="API" description="API Calls for Billboards" />
+      <DataTable searchKey="name" columns={columns} data={data} />
+      <Heading title="API" description="API Calls for Categories" />
       <Separator />
-      <ApiList entityName="billboards" entityIdName="billboardId" />
+      <ApiList entityName="categories" entityIdName="categoryId" />
     </>
   );
 };
